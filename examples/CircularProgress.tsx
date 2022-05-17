71bb8440cb114a290088e64f06a9a27544d6ffcb
Michael Szmadzinski <smujmaiku@gmail.com>
2022-05-16 22:22:38 -0400

    another fun example

---
+++ examples/CircularProgress.tsx
@@ -0,0 +1,37 @@
+import { useCallback } from 'react';
+import { useLayer, CanvasDrawInterface } from '../src';
+
+export default function CircularProgress({ color }: { color: string }): null {
+	const handleDraw = useCallback(
+		({ canvas, box, now }: CanvasDrawInterface): void => {
+			const ctx = canvas.getContext('2d');
+			if (!ctx) throw new Error();
+
+			const { width, height } = box;
+			const size = Math.min(width, height);
+
+			const progress = (now / 3000) % 1;
+			const offset = (now / 7000) % 1;
+			const start = progress > 0.5 ? 1 : progress * 2;
+			const end = progress < 0.5 ? 1 : progress * 2 - 1;
+
+			ctx.save();
+			ctx.strokeStyle = color;
+			ctx.lineWidth = size / 20;
+			ctx.beginPath();
+			ctx.arc(width / 2,
+				height / 2,
+				size * 1 / 2 - ctx.lineWidth / 2,
+				(start + offset) * 2 * Math.PI,
+				(end + offset) * 2 * Math.PI,
+			);
+			ctx.stroke();
+			ctx.restore();
+		},
+		[color]
+	);
+
+	useLayer(handleDraw);
+
+	return null;
+}
